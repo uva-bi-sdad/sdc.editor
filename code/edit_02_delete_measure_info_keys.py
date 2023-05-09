92d08bb6fd9f02865c7e5cb9735c7be16bcf6e21
Joanna <58779663+jo-schroeder@users.noreply.github.com>
2023-05-09 12:36:18 -0400

    updating file path

--- code/edit_02_delete_measure_info_keys.py
+++ code/edit_02_delete_measure_info_keys.py
@@ -90,7 +90,7 @@ def delete_measure_info_keys(root_dir, req_keys):
 if __name__ == "__main__":
     
     with urllib.request.urlopen(
-        "https://raw.githubusercontent.com/uva-bi-sdad/sdc.metadata/master/src/data_repo_structure/measure_structure.json"
+        "https://raw.githubusercontent.com/uva-bi-sdad/sdc.metadata/master/data/measure_structure.json"
     ) as url:
         req_keys = json.load(url)
     
