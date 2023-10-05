9cc2efbbbc54efe59455a6d3dce78ad716af9d51
alan <ahw9f@virginia.edu>
2023-10-05 12:00:48 -0400

    moving repeating functions in to util, adding csv manifest for valid data in preparation for downloads

--- code/edit_01_add_measure_info_keys.py
+++ code/edit_01_add_measure_info_keys.py
@@ -7,23 +7,13 @@ from pathlib import Path
 import traceback
 import settings
 from datetime import datetime
-import util
+from util import exception_handler
 from pprint import pprint
 import pandas as pd
 import os
 import sys
 import urllib.request
 
-
-def exception_handler(func):
-    def wrapper(*args, **kwargs):
-        try:
-            logging.debug("-" * 80)
-            return func(*args, **kwargs)
-        except Exception:
-            logging.debug(traceback.format_exc())
-
-    return wrapper
 
 @exception_handler
 def add_measure_info_keys(root_dir, req_keys):
@@ -33,68 +23,64 @@ def add_measure_info_keys(root_dir, req_keys):
     logging.debug("=" * 80)
 
     # CHECK EACH DATA/DISTRIBUTION FILE ------------------------
-        
+
     for path in Path(root_dir).rglob("data/distribution/**/*"):
         logging.debug("\tEvaluating: %s" % path.name)
 
         if not os.path.isfile(path):
             # if path is not a file, skip to the next file to check
             continue
-            
+
         if path.name not in ["measure_info.json"]:
             # if file is not a measure info file, skip to next
             continue
 
         # execute these statements for measure info files
-            
-        with open(path.resolve(), 'r') as f:
+
+        with open(path.resolve(), "r") as f:
             mi = json.load(f)
-       
+
         # get measure info keys for each variable
 
         updated = 0  # tracks if file is updated and needs to be rewritten
-        
+
         for var in mi.keys():
-                
             # skip references entries
             if var == "_references":
                 continue
-            
+
             key_list = list(mi[var].keys())
 
             # check for missing keys
             missing_keys = list(set(req_keys).difference(set(key_list)))
-            
+
             if len(missing_keys) > 0:
-            
                 # add missing keys
                 for i in range(len(missing_keys)):
-                    mi[var][missing_keys[i]] = ""    
-                
-                # sort keys 
+                    mi[var][missing_keys[i]] = ""
+
+                # sort keys
                 updated_keys = list(set(key_list).union(set(missing_keys)))
                 updated_keys.sort()
                 sorted_dict = {i: mi[var][i] for i in updated_keys}
-                mi[var] = sorted_dict     
-                
+                mi[var] = sorted_dict
+
                 updated = 1
-            
+
         # write back updates to measure_info file
-        if updated:      
-            with open(path.resolve(), 'w') as f:
+        if updated:
+            with open(path.resolve(), "w") as f:
                 json.dump(mi, f, indent=4)
 
-    return 
+    return
 
 
 if __name__ == "__main__":
-    
     with urllib.request.urlopen(
         "https://raw.githubusercontent.com/uva-bi-sdad/sdc.metadata/master/data/measure_structure.json"
     ) as url:
         req_keys = json.load(url)
-    
-    
+
     parser = argparse.ArgumentParser(
         description="Given a directory, adds missing keys to each measure_info.json in a data/distribution folder."
     )
@@ -119,4 +105,3 @@ if __name__ == "__main__":
         logging.info("%s is not a directory", (args.input_root))
     else:
         add_measure_info_keys(args.input_root, req_keys)
-
